42caf32481c67f22d7272be3258a3d8cce92a20a
Claude.fauconnet <claude.fauconnet@neuf.fr>
2018-10-30 11:07:41 +0100

    setup

--- bin/mariadbd.bat
+++ bin/mariadbd.bat
@@ -1,1 +1,9 @@
 "C:\Program Files\MariaDB 10.2\bin\mysqld"
+
+
+config server
+
+CREATE USER 'bailletarchives' IDENTIFIED BY 'nodeApp55';
+GRANT USAGE ON *.* TO 'bailletarchives'@localhost IDENTIFIED BY 'nodeApp55';
+GRANT ALL privileges ON 'bailletarchives'.* TO 'bailletarchives'@localhost;
+mysql> FLUSH PRIVILEGES;
